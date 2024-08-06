91420d8d6e765299699412f019610a44f85d6cc8
Brian huynh <vuduchv@gmail.com>
2024-08-06 22:15:52 +1000

    example 8

---
+++ Fortran/Project_1/example_7.f90
@@ -0,0 +1,15 @@
+program arrays
+  implicit none 
+  integer :: i
+  real, dimension(3) :: vector1, vector2
+
+  vector1 = [ 3.0, 5.0, 7.0 ]
+  vector2 = [ (0.5*i - 1.0, i=1, 3) ]
+  print *, vector1
+  print *, vector2
+  print *, vector1 + vector2
+  print *, vector1*vector2
+  print *, dot_product(vector1, vector2)
+  print *, size(vector1)
+  print *, sum(vector1)
+end program arrays
